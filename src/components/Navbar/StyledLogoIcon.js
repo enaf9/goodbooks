ec57da4c12a3ab79c06cacc9def6c9ba48903383
Karel Kvítek <karel.kvitek@seznam.cz>
2019-08-13 21:55:43 +0200

    [#1ebxk6] Phone Navbar menu fix.

--- src/components/Navbar/StyledLogoIcon.js
+++ src/components/Navbar/StyledLogoIcon.js
@@ -2,7 +2,7 @@ import { ReactComponent as Logo } from "../../images/logo-icon.svg";
 import styled from "styled-components";
 
 const StyledLogoIcon = styled(Logo)`
-  margin: 0px 16px;
+  margin: -1px 16px 0px;
   cursor: pointer;
 `;
 
