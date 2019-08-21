e700eef4bc5f2d30e7ca6907ac4dca7ed3ae9d8a
Elena Fodorova <ena.fodorova@gmail.com>
2019-08-21 19:22:29 +0200

    [#1gccem] Added animation to Tabs.

--- src/components/Tabs/Tab/UnderLine.js
+++ src/components/Tabs/Tab/UnderLine.js
@@ -2,9 +2,11 @@ import styled from "styled-components";
 
 const UnderLine = styled.hr`
   margin-top: 7px;
+  width: ${props => (props.active ? "100%" : "0px")};
   border: 1px solid
     ${props =>
       props.active ? props.theme.colors.main.lightOrange : "transparent"};
+  transition: width 0.5s ease-out;
 `;
 
 export default UnderLine;
