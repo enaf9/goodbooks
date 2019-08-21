d3d98fddaf9df6b33ba8a8271b5f626775b2f3cc
Karel Kvítek <karel.kvitek@seznam.cz>
2019-08-21 20:02:32 +0200

    [#1ekdqw] BookPage in progress - Reviews in progress.

---
+++ src/components/pages/BookPage/Reviews/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+//styled components imports
+import AddButton from "./AddButton";
+import SelectInput from "../../../SelectInput/index";
+
+const Reviews = () => {
+  return (
+    <>
+      <AddButton>PŘIDAT+</AddButton>
+      <SelectInput />
+    </>
+  );
+};
+
+export default Reviews;
