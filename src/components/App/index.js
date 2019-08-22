c3b487a3a5c29fa05b7312e71567cc4d9df55be0
Elena Fodorova <ena.fodorova@gmail.com>
2019-08-22 10:44:52 +0200

    [#1gfu2e] Author Page fix.

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Switch, Route, withRouter } from "react-router-dom";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 import GlobalStyle from "../../utils-styled-components/global";
 import { ThemeProvider } from "styled-components";
--- src/components/pages/AuthorPage/Wrapper.js
+++ src/components/pages/AuthorPage/Wrapper.js
@@ -3,6 +3,10 @@ import styled from "styled-components";
 const Wrapper = styled.div`
   max-width: 1180px;
   margin: 15px auto 0px;
+
+  @media (${props => props.theme.mediaQueries.tablet}) {
+    margin: 30px auto 0px;
+  }
 `;
 
 export default Wrapper;
