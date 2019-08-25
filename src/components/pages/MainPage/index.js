d356889c9809737e8ba2914820a9f8af4780e6aa
Karel Kvítek <karel.kvitek@seznam.cz>
2019-08-25 19:05:08 +0200

    [#1gk0u4] Implemented functionality to get books data from Firestore database - Main Page.

--- src/components/pages/MainPage/index.js
+++ src/components/pages/MainPage/index.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
 
 //styled components imports
 import StyledMainPage from "./StyledMainPage";
@@ -6,21 +7,53 @@ import Title from "./Title";
 import QuoteAuthor from "./QuoteAuthor";
 import Quote from "./Quote";
 import Wrapper from "./Wrapper";
+import LoadingWrapper from "./LoadingWrapper";
 
 //components imports
 import BookList from "./BookList/index";
 import AuthorList from "./AuthorList/index";
 import Banner from "./Banner/index";
 
+import firebase from "../../../firebase";
+import { addBook } from "../../../store/actions/booksActions";
+import { ReactComponent as Loading } from "../../../images/loading.svg";
+
 const MainPage = () => {
+  const dispatch = useDispatch();
+  const [booksLoaded, setBooksLoaded] = useState(false);
+
+  useEffect(() => {
+    const getBooks = async () => {
+      const db = firebase.firestore();
+      const snapshot = await db.collection("books").get();
+      const books = snapshot.docs;
+      for (let book of books) {
+        dispatch(addBook(book.data()));
+      }
+      setBooksLoaded(true);
+    };
+    getBooks();
+  }, []);
+
+  const renderBookList = () => {
+    if (booksLoaded) {
+      return <BookList />;
+    } else {
+      return (
+        <LoadingWrapper>
+          <Loading />
+        </LoadingWrapper>
+      );
+    }
+  };
   return (
     <StyledMainPage>
       <Banner />
       <Wrapper>
         <Title>nové knihy</Title>
-        <BookList />
+        {renderBookList()}
         <Title>aktuálně populární knihy</Title>
-        <BookList />
+        {renderBookList()}
         <Quote>
           „Knihy jsou pro lidi tím, čím jsou pro ptáky křídla.“
           <QuoteAuthor>- John Ruskin </QuoteAuthor>
--- src/store/reducers/booksReducer.js
+++ src/store/reducers/booksReducer.js
@@ -1,23 +1,12 @@
-const booksReducer = (
-  state = [
-    {
-      id: 1,
-      title: "Cesta králů",
-      author: "Brandon Sanderson",
-      coverImage:
-        "https://www.databazeknih.cz/images_books/11_/114615/big_archiv-bourne-zare-cesta-kralu-iRU-114615.jpg"
-    },
-    {
-      id: 2,
-      title: "Meč přísahy",
-      author: "Brandon Sanderson",
-      coverImage:
-        "https://www.databazeknih.cz/images_books/41_/414497/bmid_mec-prisahy-6xj-414497.jpg"
-    }
-  ],
-  action
-) => {
-  return state;
+const booksReducer = (state = [], action) => {
+  switch (action.type) {
+    case "ADD_BOOK":
+      const newState = [...state];
+      newState.push(action.book);
+      return newState;
+    default:
+      return state;
+  }
 };
 
 export default booksReducer;
