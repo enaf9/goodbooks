1bed20b2ef8b0728d92267d1a08fd8118170586f
Elena Fodorova <ena.fodorova@gmail.com>
2019-08-26 13:35:03 +0200

    [#1gjtqw] Message pop-up created.

--- src/components/pages/BookPage/BookDetailCard/BookStatusBar/index.js
+++ src/components/pages/BookPage/BookDetailCard/BookStatusBar/index.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { useSelector } from "react-redux";
 
 //styled components imports
 import HeartIcon from "../../../../../shared-styled-components/HeartIcon";
@@ -8,6 +9,9 @@ import CheckIcon from "../../../../../shared-styled-components/CheckIcon";
 import IconWrapper from "./IconWrapper"; //added to get target id from icon click (svg/path problem)
 import Wrapper from "./Wrapper";
 
+//components imports
+import SignMessage from "../../../../pop-ups/SignMessage";
+
 const BookStatusBar = () => {
   const [iconsCheck, setIconsCheck] = useState({
     heartIcon: false,
@@ -15,39 +19,49 @@ const BookStatusBar = () => {
     bookIcon: false,
     checkIcon: false
   });
+  const [message, setMessage] = useState(null);
+  const isLogged = useSelector(state => state.loggedReducer);
 
   const handleClick = e => {
-    if (e.target.id === "heartIcon") {
-      setIconsCheck({
-        ...iconsCheck,
-        [e.target.id]: !iconsCheck[e.target.id]
-      });
+    if (isLogged) {
+      if (e.target.id === "heartIcon") {
+        setIconsCheck({
+          ...iconsCheck,
+          [e.target.id]: !iconsCheck[e.target.id]
+        });
+      } else {
+        setIconsCheck({
+          ...iconsCheck,
+          bookmarkIcon: false,
+          bookIcon: false,
+          checkIcon: false,
+          [e.target.id]: !iconsCheck[e.target.id]
+        });
+      }
     } else {
-      setIconsCheck({
-        ...iconsCheck,
-        bookmarkIcon: false,
-        bookIcon: false,
-        checkIcon: false,
-        [e.target.id]: !iconsCheck[e.target.id]
-      });
+      console.log("X");
+      setMessage(<SignMessage />);
     }
   };
 
   return (
-    <Wrapper>
-      <IconWrapper id="heartIcon" onClick={handleClick}>
-        <HeartIcon checked={iconsCheck.heartIcon} />
-      </IconWrapper>
-      <IconWrapper id="bookmarkIcon" onClick={handleClick}>
-        <BookmarkPlusIcon checked={iconsCheck.bookmarkIcon} />
-      </IconWrapper>
-      <IconWrapper id="bookIcon" onClick={handleClick}>
-        <BookIcon checked={iconsCheck.bookIcon} />
-      </IconWrapper>
-      <IconWrapper id="checkIcon" onClick={handleClick}>
-        <CheckIcon checked={iconsCheck.checkIcon} />
-      </IconWrapper>
-    </Wrapper>
+    <>
+      <Wrapper>
+        <IconWrapper id="heartIcon" onClick={handleClick}>
+          <HeartIcon checked={iconsCheck.heartIcon} />
+        </IconWrapper>
+        <IconWrapper id="bookmarkIcon" onClick={handleClick}>
+          <BookmarkPlusIcon checked={iconsCheck.bookmarkIcon} />
+        </IconWrapper>
+        <IconWrapper id="bookIcon" onClick={handleClick}>
+          <BookIcon checked={iconsCheck.bookIcon} />
+        </IconWrapper>
+        <IconWrapper id="checkIcon" onClick={handleClick}>
+          <CheckIcon checked={iconsCheck.checkIcon} />
+        </IconWrapper>
+      </Wrapper>
+      {message}
+    </>
   );
 };
 
