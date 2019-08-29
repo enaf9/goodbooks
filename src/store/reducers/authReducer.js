ab94a799a07a79bd9d5b0702f89431067a83a721
Karel Kvítek <karel.kvitek@seznam.cz>
2019-08-29 20:34:43 +0200

    authReducer fix.

--- src/store/reducers/authReducer.js
+++ src/store/reducers/authReducer.js
@@ -1,11 +1,11 @@
 const authReducer = (state = { success: null, error: null }, action) => {
   switch (action.type) {
     case "SIGN_UP_SUCCESS":
-      return { ...state, user: action.success };
+      return { ...state, success: action.success };
     case "SIGN_UP_FAILURE":
       return { ...state, error: action.error };
     case "SIGN_IN_SUCCESS":
-      return { ...state, user: action.success };
+      return { ...state, success: action.success };
     case "SIGN_IN_FAILURE":
       return { ...state, error: action.error };
     default:
