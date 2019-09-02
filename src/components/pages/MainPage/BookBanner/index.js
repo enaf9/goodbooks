218a9c28c80606208bc4b62267b82c4b280943a0
Karel Kvítek <karel.kvitek@seznam.cz>
2019-09-02 18:07:48 +0200

    [#1jjm24] BookBanner clikcable.

--- src/components/pages/MainPage/BookBanner/index.js
+++ src/components/pages/MainPage/BookBanner/index.js
@@ -10,25 +10,28 @@ import Image from "./Image";
 import Title from "./Title";
 import Author from "./Author";
 import Description from "./Description";
+import StyledLink from "./StyledLink";
 
 const BookBanner = props => {
   const book = useSelector(state => state.booksReducer.newBooks[0]);
 
   const renderBook = () => {
     return props.areBooksLoaded ? (
-      <Container>
-        <Image src={book.coverImage} />
-        <div>
-          <Title>{book.title}</Title>
-          <Author>{book.author.name}</Author>
-          <Description>
-            S Hygge to nekončí. Zapojte se do revolučního tažení za štěstím!
-            Autor světového bestselleru Hygge – Prostě šťastný způsob života
-            přichází s dalšími.
-          </Description>
-          <Rating size="36" average="4.9" large />
-        </div>
-      </Container>
+      <StyledLink to={`book/${book.id}`}>
+        <Container>
+          <Image src={book.coverImage} />
+          <div>
+            <Title>{book.title}</Title>
+            <Author>{book.author.name}</Author>
+            <Description>
+              S Hygge to nekončí. Zapojte se do revolučního tažení za štěstím!
+              Autor světového bestselleru Hygge – Prostě šťastný způsob života
+              přichází s dalšími.
+            </Description>
+            <Rating size="36" average="4.9" large />
+          </div>
+        </Container>
+      </StyledLink>
     ) : null;
   };
   return <Wrapper>{renderBook()}</Wrapper>;
