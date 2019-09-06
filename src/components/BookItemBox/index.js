c83f429662df1d7e32e16d529a7e5097fe79369c
Elena Fodorova <ena.fodorova@gmail.com>
2019-09-06 21:36:07 +0200

    BookItemBox fix.

--- src/components/BookItemBox/index.js
+++ src/components/BookItemBox/index.js
@@ -8,11 +8,15 @@ import AuthorName from "./AuthorName";
 
 const BookItemBox = props => {
   return (
-    <Wrapper to={"/book/" + props.data.id && "1"}>
-      <Image src={props.data ? props.data.coverImage : ""} />
+    <Wrapper to={"/book/1"}>
+      {/* <Wrapper to={"/book/" + props.data.id }> */}
+      <Image src={""} />
+      {/* <Image src={props.data ? props.data.coverImage : ""} /> */}
       <div>
-        <BookName>{props.data ? props.data.title : ""}</BookName>
-        <AuthorName>{props.data ? props.data.author.name : ""}</AuthorName>
+        <BookName>Cesta králů</BookName>
+        {/* <BookName>{props.data.title }</BookName> */}
+        <AuthorName>Brandon Sanderson</AuthorName>
+        {/* <AuthorName>{props.data.author.name}</AuthorName> */}
       </div>
     </Wrapper>
   );
